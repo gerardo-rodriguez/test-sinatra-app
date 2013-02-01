a59f7137b5ca3c4de6233107201040d93961a2a0
Gerardo Rodriguez <ger.rod34@gmail.com>
2013-01-31 17:29:36 -0800

    Adding files.

---
+++ basics.rb
@@ -0,0 +1,43 @@
+# require 'rubygems'
+require 'sinatra'
+
+get '/' do
+	"Hello World!"
+end
+
+get '/about' do
+	'A little about me.'
+end
+
+get '/hello/:name/:city' do
+	"Hello there, #{params[:name].reverse}. Location: #{params[:city]}."
+end
+
+get '/more/*' do
+	params[:splat]
+end
+
+get '/form' do
+	erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+  params[:secret].reverse
+end
+
+not_found do
+  status 404
+  'not found here, sorry'
+end
